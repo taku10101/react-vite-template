4f593c6cd959e688a9873f258257b447d77b6222
taku10101 <taku1011ht@gmai.com>
2025-04-13 00:21:41 +0900

    wip tailwind setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import './style.css'
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 import graphqlLoader from 'vite-plugin-graphql-loader';
+import tailwindcss from '@tailwindcss/vite'
 
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
@@ -12,5 +13,5 @@ export default defineConfig({
       '@': path.resolve(dirname, 'src'),
     },
   },
-  plugins: [graphqlLoader()],
+  plugins: [tailwindcss()],
 });
